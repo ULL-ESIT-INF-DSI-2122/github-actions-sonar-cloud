d31607756d0309a1db816a2f7f32a98b7d5beb25
Eduardo Segredo <esegredo@ull.edu.es>
2021-04-10 11:28:20 +0100

    Substraction function added

--- src/basicFunctions.ts
+++ src/basicFunctions.ts
@@ -13,3 +13,19 @@
 export function add(firstNumber: number, secondNumber: number) {
   return firstNumber + secondNumber;
 }
+
+/**
+ * Substracts two numbers
+ * @param firstNumber Consists of the first numeric operand of the substraction
+ * @param secondNumber Consists of the second numeric operand of the substraction
+ * @return The substraction of the two numbers `firstNumber` and `secondNumber`
+ *
+ * Usage:
+ * ```typescript
+ * substract(7, 1) = 6
+ * substract(8.6, 3.3) = 5.3
+ * ```
+ */
+export function substract(firstNumber: number, secondNumber: number) {
+  return firstNumber - secondNumber;
+}
--- tests/basicFunctions.spec.ts
+++ tests/basicFunctions.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import {expect} from 'chai';
-import {add} from '../src/basicFunctions';
+import {add, substract} from '../src/basicFunctions';
 
 describe('add function tests', () => {
   it('add(1, 8) returns value 9', () => {
@@ -12,6 +12,23 @@ describe('add function tests', () => {
   });
 
   it('add(1.2, 3.5) returns value 4.7', () => {
-    expect(add(1.2, 3.5)).to.be.equal(4.7);
+    expect(add(1.2, 3.5)).to.be.approximately(4.7, 0.000000000001);
+  });
+});
+
+describe('substract function tests', () => {
+  it('substract(7, 1) returns value 6', () => {
+    expect(substract(7, 1)).to.be.equal(6);
+  });
+
+  it('substract(6.7, 3.1) returns value 3.6', () => {
+    expect(substract(6.7, 3.1)).to.be.approximately(3.6, 0.000000000001);
+  });
+
+  it('substract(5, 7) returns value -2', () => {
+    expect(substract(5, 7)).to.be.equal(-2);
+  });
+  it('substract(6.7, 9.8) returns value -3.1', () => {
+    expect(substract(6.7, 9.8)).to.be.approximately(-3.1, 0.000000000001);
   });
 });
